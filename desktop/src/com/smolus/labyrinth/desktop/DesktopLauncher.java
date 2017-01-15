c88ca52a4cb407be0c4b0b60fa2106017f74bc86
Jakub Smolaga <smolusstudio@gmail.com>
2017-01-15 18:00:21 +0100

    Setup

---
+++ desktop/src/com/smolus/labyrinth/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.smolus.labyrinth.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.smolus.labyrinth.LabyrinthGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LabyrinthGame(), config);
+	}
+}
---
+++ html/src/com/smolus/labyrinth/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.smolus.labyrinth.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.smolus.labyrinth.LabyrinthGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new LabyrinthGame();
+        }
+}
