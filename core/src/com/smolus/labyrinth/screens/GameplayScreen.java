c79ed6f0264db4e2558de44e1bfd327e719a5c85
Jakub Smolaga <smolusstudio@gmail.com>
2017-01-16 20:18:31 +0100

    Added empty gameplayscreen

---
+++ core/src/com/smolus/labyrinth/screens/GameplayScreen.java
@@ -0,0 +1,20 @@
+package com.smolus.labyrinth.screens;
+
+import com.smolus.labyrinth.LabyrinthGame;
+
+/**
+ * Created by SMOLUS on 2017-01-16.
+ */
+
+public class GameplayScreen extends AbstractScreen{
+
+    public GameplayScreen(LabyrinthGame game){
+        super(game);
+        init();
+    }
+
+    private void init(){
+
+    }
+
+}
